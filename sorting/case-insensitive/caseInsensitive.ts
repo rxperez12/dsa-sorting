8702a3f074b0b2fa5564315cf40157949c4120d2
Ricardo Perez <rxperez12@gmail.com>
2024-07-12 12:52:34 -0700

    Add case insensitive function

--- sorting/case-insensitive/caseInsensitive.ts
+++ sorting/case-insensitive/caseInsensitive.ts
@@ -1,7 +1,13 @@
 /** Sort array of strings in-place, case-insensitively. Returns array. */
 
 function caseInsensitiveSort(arr: string[]): string[] {
-  return [];
+  return arr.sort((a, b) => {
+    if(a.toLowerCase() < b.toLowerCase()) return -1;
+
+    if(a.toLowerCase() > b.toLowerCase()) return 1;
+
+    return 0
+  })
 }
 
 export { caseInsensitiveSort };
